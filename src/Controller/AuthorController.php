bb99581050b1e2b981db0d88fdfaaebdd83c8b45
william <wcaillere.pro@gmail.com>
2023-03-20 17:02:04 +0100

    Création et utilisation d'un service

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\AppService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +10,25 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/author')]
 class AuthorController extends AbstractController
 {
+    private AppService $service;
+
+    /**
+     * @param AppService $service
+     */
+    public function __construct(AppService $service)
+    {
+        $this->service = $service;
+    }
+
     #[Route('/', name: 'author_index')]
     public function index(): Response
     {
-        return $this->render("author/list.html.twig");
+        return $this->render("author/list.html.twig", ['authorList' => $this->service->getAuthorList()]);
     }
 
     #[Route('/{id}', name: 'author_details', requirements: ['id' => '\d+'])]
     public function details(int $id): Response
     {
-        return $this->render("author/details.html.twig", ['id' => $id]);
+        return $this->render("author/details.html.twig", ['author' => $this->service->getAuthor($id)]);
     }
 }
---
+++ src/Service/AppService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Service;
+
+use Faker\Factory;
+
+class AppService
+{
+    public function getName(): string
+    {
+        return 'App service';
+    }
+
+    public function getAuthorList(): array
+    {
+        return [
+            ['name' => 'Hugo', 'firstName' => 'Victor', 'id' => 1],
+            ['name' => 'Despentes', 'firstName' => 'Virginie', 'id' => 2],
+            ['name' => 'Sand', 'firstName' => 'Georges', 'id' => 3],
+            ['name' => 'Le Guin', 'firstName' => 'Ursula', 'id' => 4],
+            ['name' => 'Henlein', 'firstName' => 'Robert', 'id' => 5]
+        ];
+    }
+
+    public function getAuthor(int $id): array
+    {
+        $data = array_filter($this->getAuthorList(), function ($item) use ($id) {
+            return $item["id"] == $id;
+        });
+
+        $faker = Factory::create();
+
+        if (count($data) > 0) {
+            $data = $data[array_key_first($data)];
+            $data['bio'] = $faker->text(1000);
+            return $data;
+        } else {
+            return [];
+        }
+    }
+}
