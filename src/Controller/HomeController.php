102e7a48070961dc4fbca1b958037d69d8e63505
william <wcaillere.pro@gmail.com>
2023-03-20 14:27:25 +0100

    Ajout de Twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,33 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/home')]
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/hello', name: 'hello')]
-    public function index() {
-        return new Response("Hello Symfony");
+    public function index(): Response
+    {
+        return $this->render('home_hello.html.twig');
     }
 
-    #[Route("/details/{id}",
+    #[Route("/details/{id}/{name}",
         name: 'details',
         requirements: ['id' => '\d+'],
-        defaults: ['id' => '23'],
+        defaults: ['id' => '23', 'name' => 'michel'],
         methods: ['GET', 'POST'])]
-    public function details(int $id) {
-        return new Response("produit $id");
+    public function details(int $id, string $name): Response
+    {
+        return $this->render(
+            'home_details.html.twig',
+            [
+                'id' => $id,
+                'name' => $name,
+                'person' => ['name' => 'Hugo', 'firstName' => 'Victor']
+            ]
+        );
     }
 }
